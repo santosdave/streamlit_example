2628e390b4f9c7b98951e467ae2e3d0156ea61c6
santosdave <51959957+santosdave@users.noreply.github.com>
2024-03-01 10:49:27 +0300

    update streamlit UI

--- src/app.py
+++ src/app.py
@@ -1,33 +1,46 @@
 import streamlit as  st
-
+from langchain_core.messages import AIMessage, HumanMessage
 
 # respond to user input
 def get_response(user_input):
     return ("Not sure")
 
-st.set_page_config(page_title="Chat with  websites", page_icon="🤖")
 
+# Streamlit Config
+st.set_page_config(page_title="Chat with  websites", page_icon="🤖")
 st.title("Chat with websites")
 
-# APp sidebar
+# Streamlit sidebar
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
-# App Chat Input
+# Streamlit Chat Input
 user_input = st.chat_input("Type your message here...")
 
-# Starter message
-with st.chat_message("AI"):
-    st.write("Hello, how may I help you")
+# Chat History Stateful
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        # Starter message
+        AIMessage(content="Hello, how may I help you?"),
+    ]
 
+
+# User Input
 if user_input is not None and user_input != "":
     response = get_response(user_input)
-    with st.chat_message("Human"):
-        st.write(user_input)
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
+    st.session_state.chat_history.append(AIMessage(content=response))
 
-    with st.chat_message("AI"):
-        st.write(response)
+# Chat Mesages
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message,HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
 
 
 
