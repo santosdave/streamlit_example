01eac905bc510194de9322f93eac8f5126546f69
santosdave <51959957+santosdave@users.noreply.github.com>
2024-03-01 12:28:48 +0300

    minor refactor

--- src/app.py
+++ src/app.py
@@ -14,6 +14,54 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def main():
+    # Streamlit Config
+    st.set_page_config(page_title="Chat with  websites", page_icon="🤖")
+    st.title("Chat with websites")
+
+    # Streamlit sidebar
+    with st.sidebar:
+        st.header("Settings")
+        website_url = st.text_input("Website URL")
+
+
+    if website_url is None or website_url == "":
+        st.info("Please enter a website URL")
+
+    else:
+
+        # Chat History Stateful
+        if "chat_history" not in st.session_state:
+            st.session_state.chat_history = [
+                # Starter message
+                AIMessage(content="Hello, how may I help you?"),
+            ]
+        # Vector store Stateful
+        if "vectore_store" not in st.session_state:
+            st.session_state.vectore_store = get_vector_store(website_url)
+
+        
+
+        # Streamlit Chat Input
+        user_input = st.chat_input("Type your message here...")
+
+        # User Input
+        if user_input is not None and user_input != "":
+            response = get_response(user_input)
+
+            st.session_state.chat_history.append(HumanMessage(content=user_input))
+            st.session_state.chat_history.append(AIMessage(content=response))
+
+        # Chat Mesages
+        for message in st.session_state.chat_history:
+            if isinstance(message, AIMessage):
+                with st.chat_message("AI"):
+                    st.write(message.content)
+            elif isinstance(message, HumanMessage):
+                with st.chat_message("Human"):
+                    st.write(message.content)
+
+
 
 # create vector store from url
 def get_vector_store(url):
@@ -79,48 +127,6 @@ def get_response(user_input):
         })
     return response['answer']
 
-# Streamlit Config
-st.set_page_config(page_title="Chat with  websites", page_icon="🤖")
-st.title("Chat with websites")
 
-# Streamlit sidebar
-with st.sidebar:
-    st.header("Settings")
-    website_url = st.text_input("Website URL")
-
-
-if website_url is None or website_url == "":
-    st.info("Please enter a website URL")
-
-else:
-
-    # Chat History Stateful
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            # Starter message
-            AIMessage(content="Hello, how may I help you?"),
-        ]
-    # Vector store Stateful
-    if "vectore_store" not in st.session_state:
-        st.session_state.vectore_store = get_vector_store(website_url)
-
-    
-
-    # Streamlit Chat Input
-    user_input = st.chat_input("Type your message here...")
-
-    # User Input
-    if user_input is not None and user_input != "":
-        response = get_response(user_input)
-
-        st.session_state.chat_history.append(HumanMessage(content=user_input))
-        st.session_state.chat_history.append(AIMessage(content=response))
-
-    # Chat Mesages
-    for message in st.session_state.chat_history:
-        if isinstance(message, AIMessage):
-            with st.chat_message("AI"):
-                st.write(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message("Human"):
-                st.write(message.content)
+if __name__ == "__main__":
+    main()
